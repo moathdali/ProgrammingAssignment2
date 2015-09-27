91d95fff75e2d176a3ce85fec15b117e9e8acbe5
moathdali <moathdali@gmail.com>
2015-09-27 09:33:38 -0400

    ProgrammingAssignment2_Solution
    Calculating matrix inverse with caching data capability for saving
    processing time

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script is used to cache calculated matrices inverse
 
-## Write a short comment describing this function
+##makeCacheMatrix function stores a matrix and caches its value
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL   ##for cached matrix storage
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL ##cache clearing incase a new value is set
+  }
+  get <- function() x
+  setinv <- function(inver) inv <<- inver
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+##cacheginv calculates the inverse of a matrix calculated by "makeCacheMatrix"
+##incase the inverse is calculated before, the function returns the cached value
+
+##[IMPORTANT] this function requires the package "MASS" to be installed
+##so it can be universal and take any matrix of any dimention
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  library(MASS) ##Loading MASS package
+  inv <- x$getinv()
+  if(!is.null(inv)) {   ##Checking for cached data
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()  
+  inv <- ginv(data, ...)##inverse calculation incase no cached data available
+  x$setinv(inv)
+  inv
 }
